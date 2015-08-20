f668d5555eca0b5146ca2f7b54b0a24cae06f097
Alvin Wan <hi@alvinwan.com>
2015-08-20 10:26:32 -0700

    simple install via jupyter-pip

--- setup.py
+++ setup.py
@@ -1,25 +1,32 @@
 """Setup script for Notebook Ok Extension"""
 
-import sys
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
+
+try:
+    from jupyterpip import cmdclass
+except:
+    import pip, importlib
+    pip.main(['install', 'jupyter-pip'])
+    cmdclass = importlib.import_module('jupyterpip').cmdclass
 
 install_requires = [
-    
+    'jupyter-pip'
 ]
 
+EXT_NAME = 'notebook-ok-extension'
 VERSION = '0.1.0'
 
 setup(
-    name = 'notebook-ok-extension',
-    packages = ['notebook-ok-extension'],
-    version = VERSION,
-    install_requires = install_requires,
-    description = """An extension of the Jupyter Notebook that integrates testing with OK client.""",
-    author = 'Alvin Wan',
-    author_email = 'hi@alvinwan.com',
-    url = 'https://github.com/dsten/notebook-ok-extension',
-    download_url = 'https://github.com/dsten/notebook-ok-extension/archive/%s.zip' % VERSION,
-    keywords = ['data', 'tools', 'berkeley'],
-    classifiers = []
+    name                = EXT_NAME,
+    packages            = [EXT_NAME],
+    version             = VERSION,
+    install_requires    = install_requires,
+    description         = """An extension of the Jupyter Notebook that integrates testing with OK client.""",
+    cmdclass            = cmdclass(EXT_NAME, '{}/ok'.format(EXT_NAME)),
+    author              = 'Alvin Wan',
+    author_email        = 'hi@alvinwan.com',
+    url                 = 'https://github.com/dsten/notebook-ok-extension',
+    download_url        = 'https://github.com/dsten/notebook-ok-extension/archive/%s.zip' % VERSION,
+    keywords            = ['data', 'tools', 'berkeley'],
+    classifiers         = []
 )
